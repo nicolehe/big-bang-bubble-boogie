ba52ec10df6ac7084c3cb6caa479e9ca20dcae5c
nicolehe <nicool@gmail.com>
2016-04-10 16:00:41 -0400

    first commit

---
+++ Assets/Scripts/CirclesScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var createdTime : float = 0;
+
+
+
+function Start () {
+	createdTime = Time.timeSinceLevelLoad;
+}
+
+function Update () {
+	var currentTime = Time.timeSinceLevelLoad;
+	var counting = currentTime - createdTime;
+	if (counting > 2) {
+		Destroy(gameObject);
+	} 
+
+}
---
+++ Assets/Scripts/GC.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var RedCircle: GameObject;
+var GreenCircle: GameObject;
+var YellowCircle: GameObject;
+var BlueCircle: GameObject;
+var lastTime: float = 0;
+
+function Start() {
+
+}
+
+function Update() {
+    var counting = Time.timeSinceLevelLoad - lastTime;
+
+    if (counting > 5) {
+        pickColor();
+        lastTime = Time.timeSinceLevelLoad;
+    }
+
+
+}
+
+function pickColor() {
+	var colors = ['red', 'green', 'blue', 'yellow'];
+    var colorPick = colors[Random.Range(0, colors.length)];
+    print (colorPick);
+    if (colorPick == "green") {
+        Instantiate(GreenCircle, Vector3(0, 3, 0), Quaternion.identity);
+    } else if (colorPick == "red") {
+        Instantiate(RedCircle, Vector3(0, 3, 0), Quaternion.identity);
+    } else if (colorPick == "blue") {
+        Instantiate(BlueCircle, Vector3(0, 3, 0), Quaternion.identity);
+    } else {
+        Instantiate(YellowCircle, Vector3(0, 3, 0), Quaternion.identity);
+    } 
+
+}
