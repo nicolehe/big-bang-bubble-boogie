6bc6b22be71a232e15161a503bfb1bb1289627a4
nicolehe <nicool@gmail.com>
2016-04-14 16:21:59 -0400

    trying to make 2 circles after 20 sec

--- Assets/Scripts/GC.js
+++ Assets/Scripts/GC.js
@@ -13,22 +13,31 @@ function Start() {
 }
 
 function Update() {
-	//make a variable to keep track of how many seconds have elapsed since "lastTime"
+    //make a variable to keep track of how many seconds have elapsed since "lastTime"
     var counting = Time.timeSinceLevelLoad - lastTime;
     //every 5 seconds, run pickColor() and reset "lastTime"
-    if (counting > 5) {
-        pickColor();
-        lastTime = Time.timeSinceLevelLoad;
+
+    if (Time.timeSinceLevelLoad < 20) {
+
+        if (counting > 5) {
+            pickColor();
+            lastTime = Time.timeSinceLevelLoad;
+        }
+    } else {
+        if (counting > 5) {
+            pickColor2();
+            lastTime = Time.timeSinceLevelLoad;
+        }
     }
 
 
 }
 
 function pickColor() {
-	//make an array of colors, then randomly pick one from the array
-	var colors = ['red', 'green', 'blue', 'yellow'];
+    //make an array of colors, then randomly pick one from the array
+    var colors = ['red', 'green', 'blue', 'yellow'];
     var colorPick = colors[Random.Range(0, colors.length)];
-    print (colorPick);
+    print(colorPick);
     //depending on what was picked, create an instance of that color circle
     if (colorPick == "green") {
         Instantiate(GreenCircle, Vector3(0, 3, 0), Quaternion.identity);
@@ -38,9 +47,33 @@ function pickColor() {
         Instantiate(BlueCircle, Vector3(0, 3, 0), Quaternion.identity);
     } else {
         Instantiate(YellowCircle, Vector3(0, 3, 0), Quaternion.identity);
-    } 
+    }
 
 }
 
+function pickColor2() {
+    var colors = ['red', 'green', 'blue', 'yellow'];
+    var colorPick = colors[Random.Range(0, colors.length)];
+    var colorPick2 = colors[Random.Range(0, colors.length)];
+    print(colorPick);
+    //depending on what was picked, create an instance of that color circle
+    if (colorPick == "green") {
+        Instantiate(GreenCircle, Vector3(-2, 3, 0), Quaternion.identity);
+    } else if (colorPick == "red") {
+        Instantiate(RedCircle, Vector3(-2, 3, 0), Quaternion.identity);
+    } else if (colorPick == "blue") {
+        Instantiate(BlueCircle, Vector3(-2, 3, 0), Quaternion.identity);
+    } else if (colorPick == "yellow") {
+        Instantiate(YellowCircle, Vector3(-2, 3, 0), Quaternion.identity);
+    }
 
-
+    if (colorPick2 == "green") {
+        Instantiate(GreenCircle, Vector3(2, 3, 0), Quaternion.identity);
+    } else if (colorPick2 == "red") {
+        Instantiate(RedCircle, Vector3(2, 3, 0), Quaternion.identity);
+    } else if (colorPick2 == "blue") {
+        Instantiate(BlueCircle, Vector3(2, 3, 0), Quaternion.identity);
+    } else if (colorPick2 == "yellow") {
+        Instantiate(YellowCircle, Vector3(2, 3, 0), Quaternion.identity);
+    }
+}
