36de146b881b9fd6a0310856d72c53d14f551170
nicolehe <nicool@gmail.com>
2016-04-28 00:15:59 -0400

    add levels text again

---
+++ Assets/Scripts/animationsScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+
+var anim: Animation;
+
+
+
+function Start() {
+    anim = GetComponent. < Animation > ();
+
+    for (var state: AnimationState in anim) {
+        state.speed = 0.1;
+    }
+}
+
+function Update() {
+
+}
