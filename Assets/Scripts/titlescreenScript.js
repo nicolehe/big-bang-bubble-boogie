07cd3a0cf9dd1ad43ead226318a8e6cc43dee108
nicolehe <nicool@gmail.com>
2016-05-07 22:36:49 -0400

    add intro music

--- Assets/Scripts/titlescreenScript.js
+++ Assets/Scripts/titlescreenScript.js
@@ -1,15 +1,25 @@
 ﻿#pragma strict
 import UnityEngine.SceneManagement;
+var instructions: GameObject;
+var pressCount = 0;
+var pressed = false;
 
 
 
 function Start() {
-Time.timeScale = 1;
+    Time.timeScale = 1;
 }
 
 function Update() {
-    if (Input.anyKey) {
-        SceneManager.LoadScene('instructions_scene');
+	print(pressed);
+    if (Input.anyKeyDown) {
+        pressCount++;
+        if (pressCount == 1) {
+            GameObject.Instantiate(instructions, Vector3(0, 0, -1), Quaternion.identity);
+        }
+        if (pressCount == 2){
+        	SceneManager.LoadScene("countdown_scene");
+        }
     }
 
 }
